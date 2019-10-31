00d9ccfdb11fec73daadb30a4a46d7b4c5b7d522
Zoeiii <jiayigao97@gmail.com>
2019-10-31 01:22:05 -0400

    start to working on labs

---
+++ frontend/src/Lab/Dolab.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import "../App.css";
+
+class Dolab extends Component {
+  render() {
+    return <br />;
+  }
+}
+
+export default withRouter(Dolab);
