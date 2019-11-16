84c1c0f270073794fee8c2fbbe8f6e6257c6501c
Yun Seob Lee <fhzotxldj@gmail.com>
2019-11-16 00:17:52 -0500

    implement password encrypting

--- src/main/java/application/Controllers/UserController.java
+++ src/main/java/application/Controllers/UserController.java
@@ -2,8 +2,10 @@ package application.Controllers;
 
 import application.Models.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 @CrossOrigin
@@ -20,6 +22,7 @@ public class UserController {
     public int createUser(@RequestBody User user) {
         if (userRepository.findByEmail(user.getEmail()) != null) { return 1; }
         if (userRepository.findByUsername(user.getUsername()) != null) { return 2; }
+        user.setPassword(encryptPassword(user.getPassword()));
         userRepository.save(user);
         if (user.getUserType().equals("Professor")) {
             Professor professor = new Professor(user.getId());
@@ -33,9 +36,26 @@ public class UserController {
 
     @GetMapping("/login")
     public User getUser(@RequestParam(name = "email") String email, @RequestParam(name = "password") String password) {
-        User user = userRepository.findByEmailAndPassword(email, password);
+        String encrypt = encryptPassword(password);
+        User user = userRepository.findByEmailAndPassword(email, encrypt);
         if (user != null) { return user; }
-        return userRepository.findByUsernameAndPassword(email, password);
+        return userRepository.findByUsernameAndPassword(email, encrypt);
+    }
+
+    private String encryptPassword(String password) {
+        try {
+            MessageDigest sh = MessageDigest.getInstance("SHA-256");
+            sh.update(password.getBytes());
+            byte[] bytes = sh.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
